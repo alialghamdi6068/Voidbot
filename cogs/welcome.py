0ce542037845fb65cc22ecbde852dc0a18e5dd82
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-10 13:28:26 +0300

    feat: add welcome member count and inviter variables

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -7,16 +7,79 @@ from database import get_guild_data, update_guild_data, log_activity
 class Welcome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.invite_cache = {}
+
+    async def refresh_invites(self, guild):
+        try:
+            invites = await guild.invites()
+            self.invite_cache[guild.id] = {
+                invite.code: {
+                    'uses': invite.uses or 0,
+                    'inviter_id': invite.inviter.id if invite.inviter else None,
+                }
+                for invite in invites
+            }
+        except (discord.Forbidden, discord.HTTPException):
+            self.invite_cache.setdefault(guild.id, {})
+
+    async def get_inviter(self, guild):
+        old = self.invite_cache.get(guild.id, {})
+        try:
+            invites = await guild.invites()
+        except (discord.Forbidden, discord.HTTPException):
+            return None
+
+        inviter = None
+        new_cache = {}
+        for invite in invites:
+            uses = invite.uses or 0
+            old_data = old.get(invite.code, {})
+            if uses > int(old_data.get('uses', 0)) and invite.inviter:
+                inviter = invite.inviter
+            new_cache[invite.code] = {
+                'uses': uses,
+                'inviter_id': invite.inviter.id if invite.inviter else None,
+            }
+        self.invite_cache[guild.id] = new_cache
+        return inviter
+
+    def replace_variables(self, message, member, inviter=None):
+        guild = member.guild
+        count = str(guild.member_count or 0)
+        inviter_text = inviter.mention if inviter else 'غير معروف'
+        return (
+            str(message)
+            .replace('{member}', member.mention)
+            .replace('{username}', member.display_name)
+            .replace('{server}', guild.name)
+            .replace('{members}', count)
+            .replace('{count}', count)
+            .replace('{inviter}', inviter_text)
+        )
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        for guild in self.bot.guilds:
+            await self.refresh_invites(guild)
+
+    @commands.Cog.listener()
+    async def on_invite_create(self, invite):
+        await self.refresh_invites(invite.guild)
+
+    @commands.Cog.listener()
+    async def on_invite_delete(self, invite):
+        await self.refresh_invites(invite.guild)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         settings = get_guild_data(member.guild.id)
+        inviter = await self.get_inviter(member.guild)
         channel_id = settings.get('welcome_channel_id')
         if channel_id:
             channel = member.guild.get_channel(int(channel_id))
             if isinstance(channel, discord.TextChannel):
                 message = settings.get('welcome_message', 'أهلاً وسهلاً {member} في **{server}**! 👋')
-                message = message.replace('{member}', member.mention).replace('{server}', member.guild.name).replace('{count}', str(member.guild.member_count or 0))
+                message = self.replace_variables(message, member, inviter)
                 try:
                     await channel.send(message)
                 except discord.HTTPException:
